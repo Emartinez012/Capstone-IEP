206205a67d2ee4ca6ecd00b664e826db46ef8c83
Emartinez012 <edward.martinez012@mymdc.net>
2026-04-28 21:56:14 -0400

    Remove old files

--- start-app.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start "Backend" cmd /k "cd /d "c:\Users\ja373\OneDrive\Documents\Capstone EA\expert-advisor\server" && node server.js"
-timeout /t 2 /nobreak > nul
-start "Frontend" cmd /k "cd /d "c:\Users\ja373\OneDrive\Documents\Capstone EA\expert-advisor\client" && npm run dev"
-timeout /t 3 /nobreak > nul
-start http://localhost:5173
